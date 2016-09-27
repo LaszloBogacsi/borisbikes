c1b7e255a59fa53f8db8f504c544ed341f4cebc4
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-27 16:51:22 +0100

    dock bike method and testing

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,11 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
+
+
   def release_bike
     bike = Bike.new
 
   end
+def bike
+@bike
+end
 
+
+  def dock_bike(bike) #has to accept one argument from Bike
+    @bike = bike
+  end
 
 
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -16,3 +16,18 @@ describe DockingStation do
   expect(bike).to be_instance_of Bike
 end
 end
+
+describe DockingStation do
+  it { is_expected.to respond_to(:dock_bike).with(1).argument}
+
+  end
+
+describe DockingStation do
+
+  it "the passed in variable is bike" do
+    #subject {Bike.new}
+    bike = Bike.new
+  subject.dock_bike(bike)
+  expect(subject.bike).to eq bike
+  end
+end
